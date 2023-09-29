be347841aa08ef027c347a3f21cfac936c0bca7f
Vishal Kalal <vishal.rajasthan@gmail.com>
2023-09-29 09:36:53 -0500

    change unzip cmd.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "Downloading REDCap 13.8.5"
+echo "Downloading REDCap"
 wget -q -o ./redcap.zip 'https://strgredcapext.blob.core.windows.net/redcao/redcap13.8.5.zip?sp=r&st=2023-09-29T14:31:44Z&se=2023-09-29T22:31:44Z&spr=https&sv=2022-11-02&sr=b&sig=CKhjqTNyWYlsSicPYZJcha%2F6v5xcrFSQO70237tO%2BEY%3D'
-
-ls
+ls -l redcap.zip

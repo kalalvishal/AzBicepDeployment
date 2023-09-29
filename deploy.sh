2aa0430ab68e2ce4f8061e05368a3a6f29feda68
Vishal Kalal <vishal.rajasthan@gmail.com>
2023-09-29 09:18:51 -0500

    redcap zip change.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Downloading REDCap 13.8.5"
-wget -q -o ./redcap.zip 'https://strgredcapext.blob.core.windows.net/redcao/redcap13.8.5.zip?sp=r&st=2023-09-28T14:25:15Z&se=2023-09-30T22:25:15Z&spr=https&sv=2022-11-02&sr=b&sig=Bxo0a0kJ%2B8HOoWffI1gNEn1bLwtULMzFWojaOgUW%2FY4%3D'
+wget -q -o ./redcap.zip 'https://strgredcapext.blob.core.windows.net/redcao/redcap13.8.5.zip?sp=r&st=2023-09-29T14:17:47Z&se=2023-09-29T22:17:47Z&spr=https&sv=2022-11-02&sr=b&sig=krBeh6ISgB9dYtmuSevEBO7PFlWwyP3eqGKTyuU4RLA%3D'
 
 ls

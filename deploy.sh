6528110ac9d1643ad28aeb56a3add85fcfec394a
Vishal Kalal <vishal.rajasthan@gmail.com>
2023-09-28 20:42:06 -0500

    redcap sample.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Downloading REDCap 13.8.5"
-wget -q -o ../../redcap.zip 'https://strgredcapext.blob.core.windows.net/redcao/redcap13.8.5.zip?sp=r&st=2023-09-28T14:25:15Z&se=2023-09-30T22:25:15Z&spr=https&sv=2022-11-02&sr=b&sig=Bxo0a0kJ%2B8HOoWffI1gNEn1bLwtULMzFWojaOgUW%2FY4%3D'
-
+wget -q -o ./redcap.zip 'https://strgredcapext.blob.core.windows.net/redcao/redcap13.8.5.zip?sp=r&st=2023-09-28T14:25:15Z&se=2023-09-30T22:25:15Z&spr=https&sv=2022-11-02&sr=b&sig=Bxo0a0kJ%2B8HOoWffI1gNEn1bLwtULMzFWojaOgUW%2FY4%3D'
+ls
 echo "Unzipping REDCap 13.8.5"
-unzip -oq ../../redcap.zip
+unzip -oq ./redcap.zip
+ls
 
 echo "Completed"
